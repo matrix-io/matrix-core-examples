95f12a4c6eb103e9860b570ddf4f3ffe6133968e
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-07-20 19:09:39 -0400

    Update wakeword.py
    This example should now be able to send a Base port configuration message.

--- python/wakeword.py
+++ python/wakeword.py
@@ -3,16 +3,17 @@ import os # Miscellaneous operating system interface
 import zmq # Asynchronous messaging framework
 import time # Time access and conversions
 import sys # System-specific parameters and functions
+from matrix_io.proto.malos.v1 import driver_pb2 # MATRIX Protocol Buffer driver library
 from matrix_io.proto.malos.v1 import io_pb2 # MATRIX Protocol Buffer io library
 from multiprocessing import Process # Allow for multiple processes at once
 from zmq.eventloop import ioloop # Asynchronous events through ZMQ
 matrix_ip = '127.0.0.1' # Local device ip
 wakeword_port = 60001 # Driver Base port
 # Handy functions for connecting to the Data Update, & Error port 
-from utils import driver_keep_alive, register_data_callback, register_error_callback
+from utils import register_data_callback, register_error_callback
 # Sphinx Knowledge Base files
-LM_PATH = 'INSERT_PATH_TO_YOUR_FILE.lm'# Language Model File
-DIC_PATH = 'INSERT_PATH_TO_YOUR_FILE.dic'# Dictation File
+LM_PATH = '/home/pi/1339.lm'# Language Model File
+DIC_PATH = '/home/pi/1339.dic'# Dictation File
 
 ## BASE PORT ##
 def config_socket():
@@ -22,23 +23,36 @@ def config_socket():
     socket = context.socket(zmq.PUSH)
     # Connect Pusher to configuration socket
     socket.connect('tcp://{0}:{1}'.format(matrix_ip, wakeword_port))
-
+    
     # Create a new driver config
-    io_config_proto = io_pb2.WakeWordParams()
+    config = driver_pb2.DriverConfig()
     # Language Model File
-    io_config_proto.lm_path = LM_PATH
+    config.wakeword.lm_path = LM_PATH
     # Dictation File
-    io_config_proto.dic_path = DIC_PATH
+    config.wakeword.dic_path = DIC_PATH
     # Desired MATRIX microphone
-    io_config_proto.channel = 8
+    config.wakeword.channel = 8
     # Enable verbose option
-    io_config_proto.enable_verbose = False
+    config.wakeword.enable_verbose = False
 
     # Send driver configuration through ZMQ socket
-    socket.send(io_config_proto.SerializeToString())
+    socket.send(config.SerializeToString())
     print ('Listening for wakewords')
 
-## ERROR PORT ##
+## KEEP-ALIVE PORT ##
+def ping_socket():
+    # Define zmq socket
+    context = zmq.Context()
+    # Create a Pusher socket
+    ping_socket = context.socket(zmq.PUSH)
+    # Connect to the socket
+    ping_socket.connect('tcp://{0}:{1}'.format(matrix_ip, wakeword_port+1))
+    # Send a ping every 2 seconds
+    while True:
+        ping_socket.send_string('')
+        time.sleep(2)
+
+## ERROR PORT ## (!Currently Experiencing False Errors!)
 def wakeword_error_callback(error):
     # Log error
     print('{0}'.format(error))
@@ -64,8 +78,8 @@ if __name__ == '__main__':
     # Send Base Port configuration 
     config_socket()
     # Start Error Port connection
-    Process(target=register_error_callback, args=(wakeword_error_callback, matrix_ip, wakeword_port)).start()
+    # Process(target=register_error_callback, args=(wakeword_error_callback, matrix_ip, wakeword_port)).start()
     # Start Data Update Port connection
     Process(target=register_data_callback, args=(wakeword_data_callback, matrix_ip, wakeword_port)).start()
     # Start Keep-alive Port connection
-    Process(target=driver_keep_alive, args=(matrix_ip, wakeword_port)).start()
+    Process(target=ping_socket).start()
