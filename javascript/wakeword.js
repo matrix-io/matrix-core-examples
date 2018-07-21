0909b5b592673dfa73bc142ac7f017508f51a238
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-07-20 20:45:52 -0400

    Added Keep-Alive port to example

--- javascript/wakeword.js
+++ javascript/wakeword.js
@@ -25,6 +25,18 @@ var config = matrix_io.malos.v1.driver.DriverConfig.create(
 configSocket.send(matrix_io.malos.v1.driver.DriverConfig.encode(config).finish());
 console.log('Listening for wakewords');
 
+// KEEP-ALIVE PORT \\
+// Create a Pusher socket
+var pingSocket = zmq.socket('push');
+// Connect Pusher to Keep-alive port
+pingSocket.connect('tcp://' + matrix_ip + ':' + (matrix_wakeword_base_port + 1));
+// Send initial ping
+pingSocket.send('');
+// Send a ping every 2 seconds
+setInterval(function(){
+  pingSocket.send('');// Send ping
+}, 2000);
+
 // ERROR PORT \\
 // Create a Subscriber socket
 var errorSocket = zmq.socket('sub');
@@ -34,7 +46,7 @@ errorSocket.connect('tcp://' + matrix_ip + ':' + (matrix_wakeword_base_port + 2)
 errorSocket.subscribe('');
 // On Message
 errorSocket.on('message', function(error_message){
-  console.log('Error received: ' + error_message.toString('utf8'));// Log error
+  //console.log('Error received: ' + error_message.toString('utf8'));// Log error
 });
 
 // DATA UPDATE PORT \\
